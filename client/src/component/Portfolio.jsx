89eb2dac72b5fe86a5e4bed51e4a0ca8cb5d939d
Rahul-07-7 <rahuljogadiya007@gmail.com>
2026-09-15 17:06:37 +0530

    Fix incorrect link formatting in Portfolio component

--- client/src/component/Portfolio.jsx
+++ client/src/component/Portfolio.jsx
@@ -14,7 +14,7 @@ const projects = [
   },
   {
     title: "Candle-World (Ecommerce-Website)",
-    link: "https://https://candleworld.netlify.app/",
+    link: "https://candleworld.netlify.app/",
     image: Fresheat,
     description:
       "Developed a full-featured food ordering website using ReactVite, a responsive, user-friendly interface. ",
@@ -22,7 +22,7 @@ const projects = [
   {
     title: "Snacksy (Food-Website)",
     image: Fresheat,
-    link: "https://https://freshheat.netlify.app/",
+    link: "https://freshheat.netlify.app/",
     description:
       "Developed a full-featured food ordering website using React and Node.js, enabling users to browse menu items, filter by category, add items to the cart, and place orders seamlessly. Implemented client-side routing, dynamic pages, and a responsive, user-friendly interface. Integrated full authentication with email functionality, ensuring secure user access and communication.",
   },
